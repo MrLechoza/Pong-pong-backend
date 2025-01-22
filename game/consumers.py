5966531bf00db803dd4ebb2b16d20a01f8ac3d89
diego <diegogelvis14@gmail.com>
2025-01-21 20:45:37 -0400

    actualizacion

--- game/consumers.py
+++ game/consumers.py
@@ -2,7 +2,6 @@ import json
 import asyncio
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-
 class PingPongConsumer(AsyncWebsocketConsumer):
     players = {}  
     game_state = {
@@ -14,6 +13,7 @@ class PingPongConsumer(AsyncWebsocketConsumer):
     }
     
     speed_multiplier = 1.0
+    game_paused = False
 
     async def connect(self):
         self.room_group_name = "pingpong_game"
@@ -23,7 +23,6 @@ class PingPongConsumer(AsyncWebsocketConsumer):
         )
         await self.accept()
 
-        
         if len(self.players) < 2:
             role = 'player1' if 'player1' not in self.players else 'player2'
             self.players[role] = self.channel_name
@@ -56,47 +55,80 @@ class PingPongConsumer(AsyncWebsocketConsumer):
 
     async def start_game(self):
         while len(self.players) == 2:
-            ball = self.game_state["ball"]
-            ball["x"] += ball["dx"] * self.speed_multiplier
-            ball["y"] += ball["dy"] * self.speed_multiplier
+            if not self.game_paused:
+                ball = self.game_state["ball"]
+                ball["x"] += ball["dx"] * self.speed_multiplier
+                ball["y"] += ball["dy"] * self.speed_multiplier
 
-            # incrementa la velocidad 
-            self.speed_multiplier = min(self.speed_multiplier + 0.002, 4.0)
-            
-            if ball["y"] <= 0 or ball["y"] >= 100:
-                ball["dy"] *= -1
+                self.speed_multiplier = min(self.speed_multiplier + 0.002, 4.0)
                 
-            
-            if ball["x"] <= 7:  
-                if self.game_state["player1"] <= ball["y"] <= self.game_state["player1"] + 20:  
-                    ball["dx"] *= -1
-                else:
-                    self.game_state["score2"] += 1  
-                    if self.game_state["score2"] >= 5:  
-                        await self.end_game("player2")
-                        break
+                if ball["y"] <= 0 or ball["y"] >= 100:
+                    ball["dy"] *= -1
+                    
+                if ball["x"] <= 7:  
+                    if self.game_state["player1"] <= ball["y"] <= self.game_state["player1"] + 20:  
+                        ball["dx"] *= -1
                     else:
-                        self.reset_ball()
-            elif ball["x"] >= 93:  
-                if self.game_state["player2"] <= ball["y"] <= self.game_state["player2"] + 20:
-                    ball["dx"] *= -1
-                else:
-                    self.game_state["score1"] += 1  
-                    if self.game_state["score1"] >= 5: 
-                        await self.end_game("player1")
-                        break
+                        self.game_state["score2"] += 1  
+                        await self.send_score_update()
+                        if self.game_state["score2"] >= 5:  
+                            await self.end_game("player2")
+                            break
+                        else:
+                            self.reset_ball()
+                            await self.pause_game_internal()
+                elif ball["x"] >= 93:  
+                    if self.game_state["player2"] <= ball["y"] <= self.game_state["player2"] + 20:
+                        ball["dx"] *= -1
                     else:
-                        self.reset_ball()
+                        self.game_state["score1"] += 1  
+                        await self.send_score_update()
+                        if self.game_state["score1"] >= 5: 
+                            await self.end_game("player1")
+                            break
+                        else:
+                            self.reset_ball()
+                            await self.pause_game_internal()
 
-            
-            await self.channel_layer.group_send(
-                self.room_group_name,
-                {
-                    "type": "game_update",
-                    "message": self.game_state
-                }
-            )
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        "type": "game_update",
+                        "message": self.game_state
+                    }
+                )
             await asyncio.sleep(0.05)
+
+    async def send_score_update(self):
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "score_update",
+                "message": {
+                    "score1": self.game_state["score1"],
+                    "score2": self.game_state["score2"]
+                }
+            }
+        )
+
+    async def pause_game_internal(self):
+        self.game_paused = True
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "pause_game",
+                "message": "pause"
+            }
+        )
+        await asyncio.sleep(3)  # Pausa de 3 segundos
+        self.game_paused = False
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "resume_game",
+                "message": "resume"
+            }
+        )
 
     async def game_update(self, event):
         message = event["message"]
@@ -110,23 +142,40 @@ class PingPongConsumer(AsyncWebsocketConsumer):
                 "message": {
                     "winner": winner, 
                     "score1": self.game_state["score1"], 
-                    "score2": self.game_state["score2"]}
+                    "score2": self.game_state["score2"]
+                }
             }
         )
-       
         self.reset_game()
 
     def reset_ball(self):
-        
         self.game_state["ball"] = {"x": 50, "y": 50, "dx": 1, "dy": 1}
-        self.speed_multiplier = 1.0 # se restablece la velocidad
+        self.speed_multiplier = 1.0
 
     def reset_game(self):
-        
-        self.game_state["ball"] = {"x": 50, "y": 50, "dx": 1, "dy": 1}
-        self.game_state["player1"] = 50
-        self.game_state["player2"] = 50
+        self.game_state = {
+            "ball": {"x": 50, "y": 50, "dx": 1, "dy": 1},  
+            "player1": 50,  
+            "player2": 50,  
+            "score1": 0, 
+            "score2": 0, 
+        }
+        self.players = {}
+        self.speed_multiplier = 1.0
+        self.game_paused = False
 
     async def game_over(self, event):
         message = event["message"]
         await self.send(text_data=json.dumps({"type": "game_over", "message": message}))
+
+    async def score_update(self, event):
+        message = event["message"]
+        await self.send(text_data=json.dumps({"type": "score_update", "message": message}))
+
+    async def pause_game(self, event):
+        message = event["message"]
+        await self.send(text_data=json.dumps({"type": "pause_game", "message": message}))
+
+    async def resume_game(self, event):
+        message = event["message"]
+        await self.send(text_data=json.dumps({"type": "resume_game", "message": message}))
--- game/models.py
+++ game/models.py
@@ -3,3 +3,22 @@ from django.db import models
 class GameState(models.Model):
     ball_x = models.FloatField()
     ball_y = models.FloatField()
+    
+class Player(models.Model):
+    username = models.CharField(max_length=100)
+    score =  models.IntegerField(default=0)
+    joined = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.username
+    
+class Game(models.Model):
+    player1 = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='player1')
+    player2 = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='player2')
+    winner = models.ForeignKey(Player, on_delete=models.SET_NULL, related_name='winner', null=True, blank=True)
+    score_player1 = models.IntegerField(default=0)  
+    score_player2 = models.IntegerField(default=0)  
+    create = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.player1.username} vs {self.player2.username}'
